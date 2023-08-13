1d00ab9d98bbc9e0f47f0ba9b2fc042567c92c42
Bhargavsinh <bhargavsinhdodiya4@gmail.com>
2023-08-13 15:24:41 +0530

    first commit

---
+++ Form/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import LoginForm from './component/forms/Basicform';
+// import Basicform from './component/forms/Basicform';
+  
+
+
+function App() {
+  return (
+   <>
+    <div className='page'>
+    <LoginForm />
+    
+    </div>
+    
+   </>
+    
+  );
+}
+
+export default App;
